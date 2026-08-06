f8e161b25b29e8e5c8db8bae04046139710b338b
anike0900 <aniketsinha070@gmail.com>
2026-08-06 22:54:35 +0530

    feat: create user model with authentication methods

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,22 @@
-const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
-const helmet = require("helmet");
-const morgan = require("morgan");
-
 dotenv.config();
 
-const app = express();
-
-app.use(express.json());
-app.use(cors());
-app.use(helmet());
-app.use(morgan("dev"));
-
-app.get("/", (req, res) => {
-    res.json({
-        success: true,
-        message: "Xpenss Tracker API is running..."
-    });
-});
-
-
+const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`🚀 Server running on port ${PORT}`);
+        });
+
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+startServer();
